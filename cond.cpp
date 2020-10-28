ca9595deee5893adb692ba0cba3f09169a4f7fa2
Prajwal <prajwal1234patil@gmail.com>
2020-10-28 13:15:32 +0530

    butterfly pattern

--- cond.cpp
+++ cond.cpp
@@ -4,19 +4,50 @@ int main()
 {
    
     int col,row,i,j;
-    cout<<"enter the number of rows and columns "<<endl;
-    cin>>row>>col;
-    for (i=0;i<row;i++)
+    //cout<<"enter the number of rows and columns "<<endl;
+    //cin>>row>>col;
+    int n;
+    cin>>n;
+    int count=1;
+    for(i=1;i<=n;i++)
     {
-        for ( j = 0; j < col; j++)
-        {
-            cout<<"*";
 
-        }
-        cout<<"\n";
+      for(j=1;j<=i;j++)
+        cout<<"* ";
+
+    int space=2*n-2*i;
+      for (j=1;j<=space;j++)
+      {
+          cout<<"  ";
+      }
+    for(j=1;j<=i;j++)
+       {
+
+            cout<<"* ";
+       }cout<<endl;
+        
+    }
+    for(i=n;i>=1;i--)
+    {
+
+      for(j=1;j<=i;j++)
+        cout<<"* ";
+
+    int space=2*n-2*i;
+      for (j=1;j<=space;j++)
+      {
+          cout<<"  ";
+      }
+    for(j=1;j<=i;j++)
+       {
+
+            cout<<"* ";
+       }cout<<endl;
+        
+    }
+      
+    return 0;
        
         
     }
     
-
-}
