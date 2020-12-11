572db9b2ba7366dfd1c605847481b0db80a58f14
Prajwal <prajwal1234patil@gmail.com>
2020-12-11 13:27:44 +0530

    linear search

---
+++ array_prac.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std ;
+
+/* int linear(int arr[],int key, int n)  //time complexity of this program is O(n) (worst case ) because it has go through whole array 
+{
+    for (int i = 0 ; i< n;i++)
+    {
+        if (key==arr[i])
+        {
+            return i ;
+        }
+    }
+    return -1 ;
+}
+
+
+int main()
+{
+    int n ,key ;
+    
+    
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the array elements ";
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter the key "<<endl;
+    cin>>key;
+    cout<<linear( arr, n, key);
+    
+    return 0;
+}
+ */
