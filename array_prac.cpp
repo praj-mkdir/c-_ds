44edea463c94d2d9f55c909fd062ffb0df6348de
Prajwal <prajwal1234patil@gmail.com>
2020-12-12 12:27:59 +0530

    linear+binary

--- array_prac.cpp
+++ array_prac.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std ;
 
-/* int linear(int arr[],int key, int n)  //time complexity of this program is O(n) (worst case ) because it has go through whole array 
+ int linear(int arr[],int key, int n)  //time complexity of this program is O(n) (worst case ) because it has go through whole array 
 {
     for (int i = 0 ; i< n;i++)
     {
@@ -13,25 +13,64 @@ using namespace std ;
     return -1 ;
 }
 
-
-int main()
+int binn(int arr[],int n ,int key)
 {
-    int n ,key ;
-    
-    
+    int s=0;
+    int e=n;
+    int mid;
+    while (s<=e)
+    {
+        mid=(s+e)/2;
+        if (mid[arr]==key)
+        {
+            return mid ;
+        }
+        else if (mid[arr]>key)
+        {
+            e=mid-1;
+
+        }
+        else
+        {
+            s=mid+1;
+        }
+        
+        
+    }
+    return -1;
+}
+main()
+{
+    cout<<"----------------1.LINEAR SEARCH-------------"<<endl;
+    cout<<"----------------2.BINARY SEARCH--------------"<<endl;
+    cout<<"Enter the number ";
+    int number;
+    cin>>number;
+    int n ;
     cin>>n;
     int arr[n];
-    cout<<"Enter the array elements ";
-
-    
-    for (int i = 0; i < n; i++)
+    cout<<"Enter the array elements";
+    for( int i=0 ;i < n ;i++)
     {
         cin>>arr[i];
     }
+    int key;
     cout<<"Enter the key "<<endl;
     cin>>key;
-    cout<<linear( arr, n, key);
     
-    return 0;
+    switch (number)
+    {
+    case 1:
+        cout<<linear( arr, n, key);
+        break;
+    case 2:
+        cout<<binn(arr, n,key);
+        break;
+    default:
+        cout<<"SELECT FROM NUMBER FROM ABOVE"<<endl;
+        break;
+    }
+     // cout<<linear( arr, n, key);
+    //cout<<binn(arr, n,key);
 }
- */
+ 
