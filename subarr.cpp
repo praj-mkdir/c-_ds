fc786f4536bca82b4357ad365ebe732056ed3c04
Prajwal <prajwal1234patil@gmail.com>
2020-12-21 13:43:37 +0530

    max of subarray

--- subarr.cpp
+++ subarr.cpp
@@ -10,20 +10,42 @@ main()
         cin>>arr[i];
     }
     //generate the sub array 
+    int r=-1,l=-1;
+    int max=-99999;
     for ( i = 0 ; i < n; i++)
     {
         for (j  = i ; j< n; j ++)
         {
          //elements of subarray arr[i,j]
+            int sum=0;
             for ( k = i ; k <= j ; k++)
             {
-                cout<<arr[k]<<",";
+                
+               //cout<<arr[k]<<",";  
+                sum+=arr[k];
+                
+
             }
-            cout<<endl;
+            //cout<<"-->sum="<<sum<<endl;
+            if (sum>max)
+            {
+                max=sum;
+                l=i;
+                r=j;
+            }
+            
+            
         }
     
         
     }
+    //cout<<i<<j<<endl;
+    for ( k = l; k < j ; k++)
+    {
+        cout<<arr[k];
+    }
     
+    cout<<endl<<"max sum is "<<max;
     
 }
+//here 3 loops are used so there will be 3 iterations 
