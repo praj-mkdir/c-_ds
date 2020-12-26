1a0873a3a7461af20e02a3b7c5a625aa4b2ee220
Prajwal <prajwal1234patil@gmail.com>
2020-12-26 11:04:17 +0530

    Reverse array

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+main()
+{
+    int n,i;
+    cin>>n;
+    while (n>0)
+    {
+        int las;
+        las=n%10
+        i=las
+    }
+    
+}
