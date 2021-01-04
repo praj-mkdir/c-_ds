8533a2c21009fd015edb927b119b91564ab8381f
Prajwal <prajwal1234patil@gmail.com>
2021-01-04 12:57:01 +0530

    Union O(m+n)

---
+++ problems/Union_intersection.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+void un(int arr1[],int arr2[],int n,int m)
+{
+    int i=0;
+    int j=0;
+    while (i<n && j<n)
+    {
+        if (arr1[i]<arr2[j])
+        {
+            cout<<arr1[i]<<" ";
+            i++;
+        }
+        else if(arr2[j]<arr1[i])
+        {
+            cout<<arr2[j]<<" ";
+            j++;
+        }
+        else if(arr1[i]==arr2[j])
+        {
+            cout<<arr1[i]<<" ";
+            i++;j++;
+        }        
+        
+    }
+    if (i<n)
+    {
+        cout<<arr1[i]<<" ";
+        i++;
+    }
+    if (j<m)
+    {
+        cout<<arr2[j]<<" ";
+        j++;    
+    }
+    
+    
+    
+}
+main()
+{
+    int i,n; 
+    int arr1[]={ 1, 2, 4, 5, 6 };
+    int arr2[] = { 2, 3, 5, 7 };
+    n=sizeof(arr1)/sizeof(arr1[0]);
+    int m=sizeof(arr2)/sizeof(arr2[0]);
+    un(arr1,arr2,n,m);
+}
